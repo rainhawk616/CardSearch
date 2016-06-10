15d6d41a8ff56c71f440093fd56bb5ca24f28068
rainhawk616 <alexolp@gmail.com>
2016-06-09 20:00:46 -0500

    Remove db logging

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ var db = {};
 var sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     "host": process.env.DB_HOST,
     "dialect": "postgres",
-    "logging": true
+    "logging": false
 });
 
 fs
